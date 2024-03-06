a64e57a65a463c68436518cbc79c5a945d6f79c6
sm-harun <harunyuss12@gmail.com>
2024-03-06 18:48:39 +0300

    Created the project

---
+++ app/src/main/java/com/smharun/2048/MainActivity.java
@@ -0,0 +1,14 @@
+package com.smharun.2048;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
